9a01adc4c05fa424fb18acd63e4e5705d06947d8
imadeddineF <imadeddinefillalipro@gmail.com>
2025-08-29 19:30:52 +0100

    refactor:[studio] refactore studio SEO and remove unneeded files

--- studio/app/layout.tsx
+++ studio/app/layout.tsx
@@ -4,64 +4,15 @@ import { NetworkProvider } from "@/providers/network-provider";
 import { Toaster } from "@/components/ui/sonner";
 import ReactQueryProvider from "@/providers/react-query-provider";
 import { ViewTransitions } from "next-view-transitions";
-import { baseUrl } from "./sitemap";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import { AuthProvider } from "@/contexts/auth-context";
 
 export const metadata: Metadata = {
-	metadataBase: new URL(baseUrl),
 	title: {
 		default: "AeroPlanar | 3D Model Generator",
 		template: "%s | AeroPlanar",
 	},
 	description: "Generate 3D models from 2D images using AI.",
-	openGraph: {
-		title: "AeroPlanar | 3D Model Generator",
-		description: "Generate 3D models from 2D images using AI.",
-		url: baseUrl,
-		siteName: "Generate 3D models from 2D images using AI",
-		locale: "en",
-		type: "website",
-		images: [
-			{
-				url: "https://picsum.photos/800/600",
-				width: 800,
-				height: 600,
-			},
-			{
-				url: "https://picsum.photos/1800/1600",
-				width: 1800,
-				height: 1600,
-			},
-		],
-	},
-	twitter: {
-		title: "AeroPlanar | 3D Model Generator",
-		description: "Generate 3D models from 2D images using AI",
-		images: [
-			{
-				url: "https://picsum.photos/800/600",
-				width: 800,
-				height: 600,
-			},
-			{
-				url: "https://picsum.photos/1800/1600",
-				width: 1800,
-				height: 1600,
-			},
-		],
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
 };
 
 export default function RootLayout({
@@ -72,9 +23,7 @@ export default function RootLayout({
 	return (
 		<ViewTransitions>
 			<html lang="en" suppressHydrationWarning>
-				<body
-					className="font-sans antialiased"
-				>
+				<body className="font-sans antialiased">
 					<NetworkProvider>
 						<ReactQueryProvider>
 							<ThemeProvider
